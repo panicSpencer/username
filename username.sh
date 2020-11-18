64b977273c3fc82b1256e6be84f123f23d856157
panicSpencer <72724866+panicSpencer@users.noreply.github.com>
2020-11-17 22:20:07 -0800

    Update username.sh

--- username.sh
+++ username.sh
@@ -2,7 +2,7 @@
 # username.sh
 # author
 echo "Enter a password which only includes lower case letters, digits, and the underscore charachter: "
-read PASSWORD
+read USERNAME
 while echo "$ZIP" | egrep -E -v "^[0-9]{5}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid password - no caps or special charachters!"

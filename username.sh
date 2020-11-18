8da0c9045309afa38f03ae770e3b15c5fcdc35ad
panicSpencer <72724866+panicSpencer@users.noreply.github.com>
2020-11-17 22:18:34 -0800

    Create username.sh

---
+++ username.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+# zip.sh
+# author
+echo "Enter a password which only includes lower case letters, digits, and the underscore charachter: "
+read PASSWORD
+while echo "$ZIP" | egrep -E -v "^[0-9]{5}$" > /dev/null 2>&1
+do
+	echo "You must enter a valid password - no caps or special charachters!"
+	echo "Enter a password which only includes lower case letters, digits, and the underscore charachter: "
+	read PASSWORD
+done
+echo "Thank you"

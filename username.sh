f7ceff2dc78986c18bbc62297dc6457e364869d9
panicSpencer <72724866+panicSpencer@users.noreply.github.com>
2020-11-17 22:25:22 -0800

    Update username.sh

--- username.sh
+++ username.sh
@@ -2,11 +2,11 @@
 # username.sh
 # author
 echo "Enter a username which only includes lower case letters, digits, and the underscore charachter: "
-read USERNAME
-while echo "$ZIP" | egrep -E -v "^[0-9]{5}$" > /dev/null 2>&1
+read username
+while echo "$username" | egrep -v "^[a-z][a-z,0-9,_]$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username - no caps or special charachters!"
 	echo "Enter a username which only includes lower case letters, digits, and the underscore charachter: "
-	read USERNAME
+	read username
 done
 echo "Thank you"
